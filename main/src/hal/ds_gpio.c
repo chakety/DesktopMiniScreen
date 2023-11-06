40d7807f9afa75aba67f99963a576ea1c48ff689
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2023-11-06 11:31:29 -0500

    gpio

--- main/src/hal/ds_gpio.c
+++ main/src/hal/ds_gpio.c
@@ -75,4 +75,73 @@ void ds_touch_gpio_init(){
     xTaskCreate(gpio_task_example, "gpio_task_example", 2048,NULL,10,NULL);
     
     //install
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler,(viod*)GPIO_INPUT_IO_0);
+
+}
+
+void ds_screen_gpio_init(){
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io.conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set, e.g.GPIO18/19
+    io_conf.pint_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disanle pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with given settings
+    gpio_config(&io_conf);
+
+    //bit mask of the pins that you want to set, e.GPIO18/19
+    io_conf.pin_bit_mask = SCREEN_GPIO_OUTPUT_DC_SEL;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //bit mask of the pins that you want to set, e.GPIO18/19
+    io_conf.pin_bit_mask = SCREEN_GPIO_OUTPUT_RES_SEL;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+
+    io_config.intr_type = GPIO_INTR_NEGEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = SCREEN_GPIO_INTPUT_BUSY_SEL;
+    //set as input mode 
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mdoe
+    io_conf.pull_up_en =0;
+    gpio_config(&io_conf);
+    
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin 
+    gpio_isr_handler_add(SCREEN_GPIO_INTPUT_BUSY, gpio_isr_handler,(void*)SCREEN_GPIO_INTPUT_BUSY);
+}
+
+void ds_gpio_init(){
+    ds_touch_gpio_init();
+}
+
+void ds_gpio_set_scrren_cs(uint32_t level){
+    gpio_set_level(SCREEN_GPIO_OUTPUT_CS, level);
+}
+
+void ds_gpio_set_screen_dc(uint32_t level){
+    gpio_set_level(SCREEN_GPIO_OUTPUT_DC, level);
+}
+
+void ds_gpio_set_screen_rst(uint32_t level){
+    gpio_set_level(SCREEN_GPIO_OUTPUT_RES, level);
+}
+
+int ds_gpio_get_screen_busy(){
+    return gpio_get_level(SCREEN_GPIO_INTPUT_BUSY)
+}
+
+void ds_gpio_set_touch_rst(uint32_t level){
+    gpio_set_level(GPIO_OUTPUT_IO_0,level);
 }
