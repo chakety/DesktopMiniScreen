dc9b30397f83bebe95103d08c978a2e2137ba29d
chakety_pc <chakety@gmail.com>
2023-11-09 10:41:45 -0500

    update

---
+++ main/src/driver/ds_screen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+
+#include "ds_screen.h"
+#include "ds_gpio.h"
+#include "ds_spi.h"
+#include "ds_data_image.h"
